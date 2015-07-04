36141c0eb635a1e9aa74efac47c1853c14f782bb
yichen123 <chenyi330@hotmail.com>
2015-07-04 15:10:44 +0800

    fixing FSL bug(undone)

--- scripts/app.js
+++ scripts/app.js
@@ -255,34 +255,20 @@ APP.Main = (function() {
      */
     function colorizeAndScaleStories() {
 
-        var storyElements = document.querySelectorAll('.story');
-
+        var height = main.offsetHeight;
+        var windowTop = document.body.getBoundingClientRect().top;
+        var storyScores = document.querySelectorAll('.story__score');
         // It does seem awfully broad to change all the
         // colors every time!
-        for (var s = 0; s < storyElements.length; s++) {
-
-            var story = storyElements[s];
-            var score = story.querySelector('.story__score');
-            var title = story.querySelector('.story__title');
+        for (var s = 0; s < storyScores.length; s++) {
 
             // Base the scale on the y position of the score.
-            var height = main.offsetHeight;
-            var mainPosition = main.getBoundingClientRect();
-            var scoreLocation = score.getBoundingClientRect().top -
-                document.body.getBoundingClientRect().top;
-            var scale = Math.min(1, 1 - (0.05 * ((scoreLocation - 170) / height)));
-            var opacity = Math.min(1, 1 - (0.5 * ((scoreLocation - 170) / height)));
-
-            score.style.width = (scale * 40) + 'px';
-            score.style.height = (scale * 40) + 'px';
-            score.style.lineHeight = (scale * 40) + 'px';
+            var scoreLocation = storyScores[s].getBoundingClientRect().top - windowTop;
 
             // Now figure out how wide it is and use that to saturate it.
-            scoreLocation = score.getBoundingClientRect();
-            var saturation = (100 * ((scoreLocation.width - 38) / 2));
+            var saturation = (100 - scoreLocation / 20);
 
-            score.style.backgroundColor = 'hsl(42, ' + saturation + '%, 50%)';
-            title.style.opacity = opacity;
+            storyScores[s].style.backgroundColor = 'hsl(42, ' + saturation + '%, 50%)';
         }
     }
 
